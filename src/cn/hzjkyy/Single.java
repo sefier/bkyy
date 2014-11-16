59ba50ecd2c10b7617d19c6426d25767a2484647
唐明星 <mingxing_tang@163.com>
2014-11-17 03:14:17 +0800

    更改版本号

--- src/cn/hzjkyy/Single.java
+++ src/cn/hzjkyy/Single.java
@@ -12,7 +12,7 @@ import cn.hzjkyy.model.Plan;
 import cn.hzjkyy.tool.Log;
 
 public class Single {
-	public static String programVersion = "1116";
+	public static String programVersion = "1117";
 	public static void main(String[] args){
 		//程序运行环境
 		boolean isTest = false;
