32cf50bcadd8b2eaebdb72a96115a5cc03a39974
唐明星 <mingxing_tang@163.com>
2014-11-26 07:53:51 +0800

    短信验证码已发送

--- src/cn/hzjkyy/parser/SendParser.java
+++ src/cn/hzjkyy/parser/SendParser.java
@@ -3,7 +3,7 @@ package cn.hzjkyy.parser;
 public class SendParser extends Parser {
 	public void parse(String response) {
 		clear();
-		if(response.contains("短信已发送")){
+		if(response.contains("短信已发送") || response.contains("短信验证码已发送至你的手机")){
 			getStatusPanel().success();
 		}else{
 			getStatusPanel().error();
