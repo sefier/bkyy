ff651487d847053ff6f1aea7b55f5999271be14a
唐明星 <mingxing_tang@163.com>
2014-12-19 07:41:12 +0800

    优化程序逻辑

--- src/cn/hzjkyy/parser/ExamParser.java
+++ src/cn/hzjkyy/parser/ExamParser.java
@@ -85,10 +85,15 @@ public class ExamParser extends Parser{
 							
 							exam = new Exam(kscc, ksdd, ksrq, sysj * 1000);
 							//如果预约时间预计会超过9点08秒，并且选的是枫桦路，那么其中70%要改选江涵路
-							Calendar calendar = new GregorianCalendar();
-							int second = calendar.get(Calendar.SECOND);
-							if(second + sysj > 8 && ksdd.equals("3301034") && plan.getId() % 10 < 7){
-								exam.ksdd = "3301022";
+							if(ksdd.equals("3301034") && plan.getId() % 10 < 7){
+								Calendar calendar = new GregorianCalendar();
+								int hour = calendar.get(Calendar.HOUR_OF_DAY);
+								int minute = calendar.get(Calendar.MINUTE);
+								int second = calendar.get(Calendar.SECOND);
+								
+								if(hour == 9 && minute == 0 && second + sysj > 8){
+									exam.ksdd = "3301022";
+								}
 							}
 
 							break;
