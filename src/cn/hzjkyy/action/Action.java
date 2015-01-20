028e307dddf2adb241de8d029bf13c4dad0cd5c2
唐明星 <mingxing_tang@163.com>
2015-01-21 01:52:16 +0800

    驾校名额已满不会导致终止预约，已进行预约确认

--- src/cn/hzjkyy/action/Action.java
+++ src/cn/hzjkyy/action/Action.java
@@ -381,9 +381,9 @@ public class Action {
 				throw new SuccessException(m.find() ? m.group() : "2015-01-01");
 			}
 			
-			if(response.getResponseBody().contains("请在次月再行预约")){
-				throw new StopException("驾校名额已满");
-			}
+//			if(response.getResponseBody().contains("请在次月再行预约")){
+//				throw new StopException("驾校名额已满");
+//			}
 
 			if(response.getStatusPanel().isSuccess() && (response.getResponseBody().contains("您已预约成功"))){
 				actionLog.record("预约考试成功！");
